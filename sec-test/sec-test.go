d4b56cd88c9b8081ffbf9701d67a92bf17f43621
Kasvi <kasvi.luthra@safetyculture.io>
2025-05-02 12:26:06 +1000

    Add sec-test folder

---
+++ sec-test/sec-test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+	connStr := "host=127.0.0.1 port=5015 user=pgx_md5 password=secret dbname=pgx_test sslmode=disable"
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var version string
+	err = conn.QueryRow(context.Background(), "SELECT version();").Scan(&version)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to query version: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Postgres version:", version)
+}
